2f1738fb866602ad39f6006d45d4a71e1904f289
Sloomey <liam.dillon0308@gmail.com>
2020-05-07 19:26:38 -0700

    Added names to people, factions, and worlds.

--- src/names.py
+++ src/names.py
@@ -1,3 +1,76 @@
 """ List of all names for things that will be randomly chosen.
     This includes names of factions, characters, etc. """
 
+# List of all first names of  female characters
+char_female = ['Skyler','Alexa','Ellie','Kayla','Torrey','Gina','Margo',
+                'Sally','Daisey','Talie','Kaitlyn','Maria','Kayla','Angie',
+                'Caden','Sosie','Anabell','Kadi','Abby','Lilly','Mistie',
+                'Cecila','Ilda','Paige','Emma','Isabelle','Emily','Camila',
+                'Scarlett','Victoria','Luna','Riley','Zoey','Hannah',
+                'Violet','Ariana','Eva','Julia','Madeline','Jade',
+                'Rose','Kylie','Ashley','Bailey','Amaya','London',
+                'Juliette','Grace','Jocelyn','Margaret','Fiona']
+
+# List of all first names of male characters
+char_male = ['Arron','Kenton','Benjamin','Charlie','Jason','Timmy','Rick',
+              'Jake','Myles','Luke','Malcolm','Pablo','Julian','James','Bart',
+              'Todd','Chris','Brad','Oliver','Roy','Paul','Peter','Jim',
+              'Johnny','Gary','Damian','Jerry','Bob','Arnold','Cody',
+              'Harris','Brandon','Kendrick','Vincent','Jarvis','Stewart',
+              'Ralph','Perry','Parsa','Hector','Randy','Greg','Jordan',
+              'Manny','Joseph','Danny','Zack','Melvin','George','Matt',
+              'Micheal']
+
+# List of all last names of characters
+char_last = ['Torres','Lopez','Smith','Ronald','Homes','Peres','Ramos',
+             'Rivera','Smith','Johnson','Williams','Jones','Davis',
+             'Brown','Wilson','Thomas','Anderson','Jackson','Lee','Sanchez',
+             'Ramirez','Lewis','Scott','Hill','Flores','Green','Adams',
+             'Nelson','Baker','Carter','Gomez','Evans','Turner','Diaz',
+             'Edwards','Collins','Reyes','Murphy','Morgan','Kelly','Howard',
+             'James','Bennett','Jenkins','Barnes','Fisher','Simmons','Hamilton',
+             'Graham','Griffin','West','Gibson','Stevens','Ford','Castro',
+             'Woods','Kennedy','Olson','Silva','Dixon','Hunt','Wagner','Grant',
+             'Arnold','Pierce','Hansen','Elliott','Hudson','Delgado','Perkins',
+             'Lawrence','Greene','Burke','Morrison','Harvey','Howell','Garrett',
+             'Weber','Banks','Schultz','Fowler','Davidson','Holland','Cortez',
+             'Lambert','Rhodes','Shelton','Walters','Steele','Holt','Benson',
+             'Warner','Zimmerman','Reeves','Erickson','Baldwin','Higgins',
+             'Cummings','Robbins','Swanson','Adkins','Fischer']
+
+#List of faction first word in faction names
+faction_first = 'The'
+
+# List of all faction second words in faction names 
+faction_second = ['Glossy','Unadvised','Boundless','Graceful','Irritating',
+                  'Mysterious','Dependent','Diligent','Shocking','Cooperative',
+                  'Deserted','Creepy','Precious','Famous','Complex','Naive',
+                  'Hellish','Horrible','Bizarre','Smelly','Bloody','Productive',
+                  'Attractive','Unsuitable','Nervous','Faithful','Wandering',
+                  'Unsuitable','Terrible','Fearful','Fortunate','Enchanting',
+                  'Gifted','Nutty','Classy','Busy','Annoyed','Foolish',
+                  'Wonderful','Natural','Unknown','Mellow','Exciting',
+                  'Angry','Intelligent','Alleged','Threatening','Upset',
+                  'Untidy','Handy','Happy']
+
+# List of all faction third words in faction names 
+faction_third = ['Few','Children','Babies','Animals','Monsters','Villagers',
+                 'Nation','Humans','Cast','Strangers','Survivors','Army',
+                 'Family','Tribe','Clan','Group','Generation','Siblings',
+                 'Folks','Cousins','Descendants','Lineage','Household','People',
+                 'Race','Dynasty','Bloodline','Division','Royalty','Empire',
+                 'Reign','Dominion','Residents','Psychopaths','Lunatics',
+                 'Thinkers','Pioneers','Colonists','Settlers','Colony',
+                 'Dwellers','Commoners','Natives','Residents','Inhabitants',
+                 'Cult','Gang','Flock','Mob','Crew','Party']
+
+# List of all world names
+world_name = ['Xandolla','Miherus','Enzeustea','Xuccoth','Rabbozuno',
+              'Ustreonides','Ochatis','Thonzaolia','Subbov','Batis','Chalveron',
+              'Dacarro','Relara','Beunov','Vubaria','Goulea','Sanvurn',
+              'Nothotune','Unrichi','Cazetera','Meuhines','Azohines','Tebrerth',
+              'Meithea','Strolinus','Motanov','Chithohines','Tibbolla',
+              'Thumitera','Gramorilia','Zolrueturn','Ezeuliv','Calrarth',
+              'Lognadus','Tharuta','Yicarro','Llusotera','Chigenia','Hunvyke'
+              'Cennoth','Uarilia','Thiobos','Phehothea','Vaneter','Vistriarus',
+              'Lugrinda','Zomilia','Cotania','Cuburia','Vemao','Drivoter']
